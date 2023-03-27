28322623190c095b1555699c37e5d42d6497df7c
Mike <mike.munene@student.moringaschool.com>
2023-03-27 11:23:24 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    newTask(document.getElementById('new-task-description').value);
+
+    form.reset();
+  })
+
 });
+
+function newTask(task) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete)
+
+  li.textContent = `${task} `;
+  btn.textContent = 'x';
+  console.log(li);
+
+  document.querySelector('#tasks').appendChild(li);
+  li.appendChild(btn);
+  // document.getElementsByTagName(li).appendChild(btn);
+}
+
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
